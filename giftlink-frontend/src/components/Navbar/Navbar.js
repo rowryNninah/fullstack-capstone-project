6e153020f46308dd464e63d421be12c6cc5c4ef6
rowryNninah <rorisang.mosapa@umuzi.org>
2025-09-17 17:53:50 -0400

    added search bar

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -10,6 +10,7 @@ export default function Navbar() {
                     {/* Task 1: Add links to Home and Gifts below*/}
                     <li className='nav-item'><a className='nav-link' href="/home.html">Home</a></li>
                     <li className='nav-item'><a className='nav-link' href="/app">Gifts</a></li>
+                    <li className="nav-item"><Link className="nav-link" to="/app/search">Search</Link></li>
                 </ul>
             </div>
         </nav>
